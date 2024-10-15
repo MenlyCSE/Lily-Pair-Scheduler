8e793cbebfc847021e799f76848c5c295290a063
MenlyCSE <MenlyCSE@gmail.com>
2024-10-15 18:30:14 -0400

    refactoring timezone function

--- js/timezones.js
+++ js/timezones.js
@@ -1,23 +1,20 @@
 function displayTimezones() {
     const selectMenu = document.getElementById(`select-menu`);
-    const timezoneAPI = `https://timeapi.io/api/timezone/availabletimezones`;
+    const popularTimezones = [
+        "America/New_York", "America/Chicago", "America/Denver",
+        "America/Los_Angeles", "America/Seattle", "America/Toronto",
+        "America/Vancouver", "Europe/London", "Europe/Berlin",
+        "Europe/Paris", "Asia/Tokyo", "Asia/Hong_Kong",
+        "Asia/Singapore", "Australia/Sydney", "Australia/Melbourne",
+        "Pacific/Auckland", "America/Sao_Paulo", "Europe/Moscow",
+        "Africa/Johannesburg", "Asia/Shanghai", "Asia/Calcutta",
+    ];
 
-    try {
-        fetch(timezoneAPI)
-        .then(response => response.json())
-        .then(data => {
-            const result = data.map(timezone => 
-                `<option value="${timezone}">${timezone}</option>`
-            ).join('');
+    const result = popularTimezones
+        .map(zone =>`<option value="${zone}">${zone}</option>`)
+        .join('');
 
-            selectMenu.innerHTML += result;
-        })
-
-    } catch (error)  {
-        return;
-    }
+    selectMenu.innerHTML += result;
 }
 
 displayTimezones();
-
-
