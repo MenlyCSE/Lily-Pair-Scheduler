d9123172564cb9f7f58290f5c70d583ae2325fad
MenlyCSE <MenlyCSE@gmail.com>
2024-10-15 17:38:11 -0400

    refactoring cards

--- js/generateCards.js
+++ js/generateCards.js
@@ -1,85 +1,90 @@
-let cardContainer = document.getElementById('card-container');
-
-let data = {
-    'Abraham Lincoln': {
+const cardContainer = document.getElementById('card-container');
+const data = {
+    'Abraham': {
         type: 'tutor',
         available: 'Mondays, Tuesdays, Fridays, and Sundays',
         timezone: 'Mountain Standard Time'
     },
-    'Jessenia Chan': {
+    'Jessenia': {
         type: 'student',
         available: 'Sundays, Mondays, and Tuesday',
         timezone: 'Pacific Standard Time'
     },
-    'Lily Guo': {
+    'Lily': {
         type: 'tutor',
         available: 'Mondays, Fridays, and Saturdays',
         timezone: 'Pacific Standard Time'
     },
-    'Jiaming Guan': {
+    'Jiaming': {
         type: 'student',
         available: 'Tuesdays and Thursdays',
         timezone: 'Pacific Standard Time'
     },
-    'Joyce Chen': {
+    'Joyce': {
         type: 'tutor',
         available: 'Wednesdays, Fridays and Sundays',
         timezone: 'Japan Standard Time'
     },
-    'Menly Cherisme': {
+    'Menly': {
         type: 'student',
         available: 'Sundays and Fridays',
         timezone: 'Indian Standard Time'
     },
-    'Sarah Flutter': {
+    'Sarah': {
         type: 'tutor',
         available: 'Mondays and Fridays',
         timezone: 'British Summer Time'
     },
-    'Quincy Goleman': {
+    'Quincy': {
         type: 'tutor',
         available: 'Mondays, Tuesdays and Thursdays',
         timezone: 'Central Standard Time'
     }
 };
 
-function displayCards() {
-    for (person in data) {
-        const { type, available, timezone } = data[person];
-
-        if (type == 'tutor')
-            cardContainer.innerHTML += `
-                <div class="card card--primary">
-                    <header class="card__header">
-                      <h3>${person.length > 15 ? person.slice(0, 10) + '...' : person}</h3>
-                    </header>
-                    <div class="card__body">
-                      <div class="list list--primary">
-                        <li class="list__item">A ${type}, ready to help</li>
-                        <li class="list__item">${available}</li>
-                        <li class="list__item">${timezone}</li>
-                      </div>
-                      <input type="submit" value="Pair" class="btn btn--primary" />
-                    </div>
-                </div>`;
-
-
-        if (type == 'student')
-            cardContainer.innerHTML += `
-                <div class="card card--secondary">
-                    <header class="card__header">
-                      <h3>${person}</h3>
-                    </header>
-                    <div class="card__body">
-                      <div class="list list--primary">
-                        <li class="list__item">A ${type}, looking for a tutor</li>
-                        <li class="list__item">${available}</li>
-                        <li class="list__item">${timezone}</li>
-                      </div>
-                      <input type="submit" value="Pair" class="btn btn--secondary" />
-                    </div>
-                </div>`;
-    }
+function displayTutorCards() {
+    return Object.keys(data)
+            .filter(person => data[person].type === 'tutor')
+            .map(person => `
+                    <div class="card card--primary">
+                        <header class="card__header">
+                          <h3>${person}</h3>
+                        </header>
+                        <div class="card__body">
+                          <div class="list list--primary">
+                            <li class="list__item">A ${data[person].type} ready to help</li>
+                            <li class="list__item">${data[person].available}</li>
+                            <li class="list__item">${data[person].timezone}</li>
+                          </div>
+                          <input type="submit" value="Pair" class="btn btn--primary" />
+                        </div>
+                    </div>`
+            ).join('')
 }
 
-displayCards();
+function displayStudentCards() {
+    return Object.keys(data)
+            .filter(person => data[person].type === 'student')
+            .map(person => `
+                    <div class="card card--secondary">
+                        <header class="card__header">
+                          <h3>${person}</h3>
+                        </header>
+                        <div class="card__body">
+                          <div class="list list--primary">
+                            <li class="list__item">A ${data[person].type} looking for a tutor</li>
+                            <li class="list__item">${data[person].available}</li>
+                            <li class="list__item">${data[person].timezone}</li>
+                          </div>
+                          <input type="submit" value="Pair" class="btn btn--secondary" />
+                        </div>
+                    </div>`
+            ).join('')
+}
+
+function showCards() {
+    cardContainer.innerHTML += displayStudentCards();
+    cardContainer.innerHTML += displayTutorCards();
+}
+
+showCards();
