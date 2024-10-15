cf4c54fb616db9d574bb51e9fe191469d315367a
MenlyCSE <MenlyCSE@gmail.com>
2024-10-14 22:34:21 -0400

    building parcel

--- js/getEonPairData.js
+++
@@ -1,3 +0,0 @@
-const sheetId = `1z0NOcAsdUuLUN3OuMp8vLbd0rptOPSjJejvHRsDe-Qc`;
-const sheetName = encodeURIComponent(`Data`);
-const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
