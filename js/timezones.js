b64a8afa67931da5089e053276a8771553a97bb9
MenlyCSE <MenlyCSE@gmail.com>
2024-10-16 21:20:51 -0400

    Refactoring timezones

--- js/timezones.js
+++ js/timezones.js
@@ -1,14 +1,16 @@
 function displayTimezones() {
     const selectMenu = document.getElementById(`select-menu`);
     const popularTimezones = [
-        "America/New_York", "America/Chicago", "America/Denver",
-        "America/Los_Angeles", "America/Seattle", "America/Toronto",
-        "America/Vancouver", "Europe/London", "Europe/Berlin",
-        "Europe/Paris", "Asia/Tokyo", "Asia/Hong_Kong",
-        "Asia/Singapore", "Australia/Sydney", "Australia/Melbourne",
-        "Pacific/Auckland", "America/Sao_Paulo", "Europe/Moscow",
-        "Africa/Johannesburg", "Asia/Shanghai", "Asia/Calcutta",
+        "Eastern Standard Time (EST)", "Central Standard Time (CST)",
+        "Mountain Standard Time (MST)", "Pacific Standard Time (PST)",
+        "British Summer Time (BST)", "Central European Time (CET)",
+        "Japan Standard Time (JST)", "Australian Eastern Standard Time (AEST)",
+        "Brasilia Time (BRT)", "Moscow Standard Time (MSK)",
+        "South Africa Standard Time (SAST)", "China Standard Time (CST)",
+        "New Zealand Standard Time (NZST)", "India Standard Time (IST)",
+        "Singapore Standard Time (SGT)",  "Hong Kong Time (HKT)"
     ];
+    
 
     const result = popularTimezones
         .map(zone =>`<option value="${zone}">${zone}</option>`)
