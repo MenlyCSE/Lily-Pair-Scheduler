10c49e793830cf24d3cc09ba620ad9f639819cbc
MenlyCSE <MenlyCSE@gmail.com>
2024-11-06 16:08:52 -0500

    connecting to excel database

--- js/generateCards.js
+++ js/generateCards.js
@@ -3,107 +3,68 @@ const timezone = document.getElementById('timezone-filter');
 const type = document.getElementById('type-filter');
 const available = document.getElementById('available-filter');
 
-// Method: organize into object, include fetch/post functions, etc
+function displayTutorCard(name, type, availability, timezone) {
+    return cardContainer.innerHTML += `
+        <div class="card card--primary">
+            <header class="card__header">
+              <h3>${name}</h3>
+            </header>
+            <div class="card__body">
+              <div class="list list--primary">
+                <li class="list__item">A ${type} ready to help</li>
+                <li class="list__item">${availability}</li>
+                <li class="list__item">${timezone}</li>
+              </div>
+              <input type="submit" value="Pair" class="btn btn--primary" />
+            </div>
+        </div>`;
+}
+
+function displayStudentCard(name, type, availability, timezone) {
+    return cardContainer.innerHTML += `
+        <div class="card card--secondary">
+            <header class="card__header">
+              <h3>${name}</h3>
+            </header>
+            <div class="card__body">
+              <div class="list list--primary">
+                <li class="list__item">A ${type} ready to help</li>
+                <li class="list__item">${availability}</li>
+                <li class="list__item">${timezone}</li>
+              </div>
+              <input type="submit" value="Pair" class="btn btn--secondary" />
+            </div>
+        </div>`;
+}
+
 async function fetchSheetData() {
     try {
         const webAppURL = `https://script.google.com/macros/s/AKfycbwUDAAIYTwGhtEh6EgoWce7aAxUFXO35DyUorX_4yG2kZzlO-CzxUBx8op50mKIBpo/exec`;
         const response = await fetch(webAppURL);
         const data = await response.json();
 
-        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            let name = data[i][0];
+            let email = data[i][1];
+            let weChatID = data[i][2];
+            let type = data[i][3];
+            let availability = data[i][4];
+            let timezone = data[i][5];
+
+            if (type === 'Student') {
+                displayStudentCard(name, type, availability, timezone);
+            }
+
+            if (type == 'Tutor') {
+                displayTutorCard(name, type, availability, timezone);
+            }
+        }
+
     } catch (error) {
         console.log(error);
     }
 }
 
-fetchSheetData(); 
+fetchSheetData();
 
-const data = {
-    'Abraham': {
-        type: 'tutor',
-        available: 'Mondays, Tuesdays, Fridays, and Sundays',
-        timezone: 'Mountain Standard Time'
-    },
-    'Jessenia': {
-        type: 'student',
-        available: 'Sundays, Mondays, and Tuesday',
-        timezone: 'Pacific Standard Time'
-    },
-    'Lily': {
-        type: 'tutor',
-        available: 'Mondays, Fridays, and Saturdays',
-        timezone: 'Pacific Standard Time'
-    },
-    'Jiaming': {
-        type: 'student',
-        available: 'Tuesdays and Thursdays',
-        timezone: 'Pacific Standard Time'
-    },
-    'Joyce': {
-        type: 'tutor',
-        available: 'Wednesdays, Fridays and Sundays',
-        timezone: 'Japan Standard Time'
-    },
-    'Menly': {
-        type: 'student',
-        available: 'Sundays and Fridays',
-        timezone: 'Indian Standard Time'
-    },
-    'Sarah': {
-        type: 'tutor',
-        available: 'Mondays and Fridays',
-        timezone: 'British Summer Time'
-    },
-    'Quincy': {
-        type: 'tutor',
-        available: 'Mondays, Tuesdays and Thursdays',
-        timezone: 'Central Standard Time'
-    }
-};
 
-function displayTutorCards() {
-    return Object.keys(data)
-            .filter(person => data[person].type === 'tutor')
-            .map(person => `
-                    <div class="card card--primary">
-                        <header class="card__header">
-                          <h3>${person}</h3>
-                        </header>
-                        <div class="card__body">
-                          <div class="list list--primary">
-                            <li class="list__item">A ${data[person].type} ready to help</li>
-                            <li class="list__item">${data[person].available}</li>
-                            <li class="list__item">${data[person].timezone}</li>
-                          </div>
-                          <input type="submit" value="Pair" class="btn btn--primary" />
-                        </div>
-                    </div>`
-            ).join('')
-}
-
-function displayStudentCards() {
-    return Object.keys(data)
-            .filter(person => data[person].type === 'student')
-            .map(person => `
-                    <div class="card card--secondary">
-                        <header class="card__header">
-                          <h3>${person}</h3>
-                        </header>
-                        <div class="card__body">
-                          <div class="list list--primary">
-                            <li class="list__item">A ${data[person].type} looking for a tutor</li>
-                            <li class="list__item">${data[person].available}</li>
-                            <li class="list__item">${data[person].timezone}</li>
-                          </div>
-                          <input type="submit" value="Pair" class="btn btn--secondary" />
-                        </div>
-                    </div>`
-            ).join('')
-}
-
-function showCards() {
-    cardContainer.innerHTML += displayStudentCards();
-    cardContainer.innerHTML += displayTutorCards();
-}
-
-showCards();
